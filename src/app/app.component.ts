a21919a499e58f2bed4e46f9fb16ef5f5bd5f2eb
MehmetAyan <mehmetayan84@gmail.com>
2018-01-18 00:51:30 +0300

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit{
+  genders = ['male', 'female'];
+  signUpForm: FormGroup;
+
+  ngOnInit() {
+    this.signUpForm = new FormGroup({
+      'userData': new FormGroup({
+        'username': new FormControl(null, Validators.required),
+        'email': new FormControl(null, [Validators.required, Validators.email])
+      }),
+      'gender': new FormControl(this.genders[0])
+    });
+  }
+
+  onSubmit() {
+    console.log(this.signUpForm);
+  }
+}
